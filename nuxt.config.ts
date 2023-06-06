ec0bd5f908cb9e3bfce3e7d8a85e7b8c41e1ca64
Jommar Ilagan <jom102386@gmail.com>
2023-06-06 12:28:30 +0800

    Adding title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Story Writer: AI',
+    },
+  },
   css: ['vuetify/lib/styles/main.sass'],
   build: {
     transpile: ['vuetify'],
